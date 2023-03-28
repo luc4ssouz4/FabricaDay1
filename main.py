5e9a7a74a825616cc10979ad05114c75e60b9c47
Lucas Souza <129099425+luc4ssouz4@users.noreply.github.com>
2023-03-28 14:10:17 -0300

    Update main.py
    Updates

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ def maximo(*valor:int):
 
     return valor
 
-n1 = input("Digite o primeiro numero: ")
-n2 = input("Digite o segundo numero: ")
-
-print("O numero maior é: "+maximo(n1, n2))
+#DETECTAR QUAL NUMERO É MAIOR, EXEMPLO(10 e 15)
+print("O numero maior é:", maximo(10, 15))
 
 
 #EXERCICIO 2
@@ -25,14 +23,14 @@ def mult(*valor:int):
         n3 = True  
 
     return n3
-
+#DETECTAR SE O NUMERO É MULTIPLO, EXEMPLO(35 e 7)
 print(mult(35,7))
 
-#EXERCICIO 3
 
+#EXERCICIO 3
 def quad(n1:int):
     area = n1*n1
     return area
-
+#DETECTAR A AREA DE UM QUADRADO, EXEMPLO (3.25)
 print(f'{quad(3.25):.2f}')
 
